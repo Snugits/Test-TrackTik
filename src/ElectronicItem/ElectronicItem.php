8f9ccb9f7271d58b067012d7d9894bfb2d08554d
Myagkov Timur <t.myagkov@gmail.com>
2021-03-26 19:51:07 +0300

    Init

---
+++ src/ElectronicItem/ElectronicItem.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\ElectronicItem;
+
+class ElectronicItem
+{
+    /**
+     * @var float
+     */
+    public $price;
+
+    /**
+     * @var string
+     */
+    private $type;
+    public $wired;
+
+    const ELECTRONIC_ITEM_TELEVISION = 'television';
+    const ELECTRONIC_ITEM_CONSOLE = 'console';
+    const ELECTRONIC_ITEM_MICROWAVE = 'microwave';
+
+    private static $types = array(
+        self::ELECTRONIC_ITEM_CONSOLE,
+        self::ELECTRONIC_ITEM_TELEVISION,
+        self::ELECTRONIC_ITEM_MICROWAVE
+    );
+
+    function getPrice()
+    {
+        return $this->price;
+    }
+
+    function getType()
+    {
+        return $this->type;
+    }
+
+    function getWired()
+    {
+        return $this->wired;
+    }
+
+    function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    function setWired($wired)
+    {
+        $this->wired = $wired;
+    }
+}
